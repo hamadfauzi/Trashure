c98b4dfaca387a3091ee19d8f68b3e73382e62eb
hamadfauzi <fauzihamad37@gmail.com>
2019-07-27 16:59:16 +0700

    fix layout

--- app/src/main/java/com/example/trashure/Feature/Beranda/BerandaFragment.java
+++ app/src/main/java/com/example/trashure/Feature/Beranda/BerandaFragment.java
@@ -3,6 +3,7 @@ package com.example.trashure.Feature.Beranda;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -10,10 +11,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.trashure.Feature.Notifikasi.NotifikasiFragment;
 import com.example.trashure.Feature.Setting.SettingFragment;
 import com.example.trashure.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class BerandaFragment extends Fragment{
@@ -22,6 +30,10 @@ public class BerandaFragment extends Fragment{
     private NotifikasiFragment notifikasiFragment;
     private SettingFragment settingFragment;
     private BottomNavigationView bottomNavigationView;
+    private FirebaseAuth mAuth;
+    private String currentUserID;
+    private DatabaseReference userRefs;
+    private TextView tvSaldo,tvLevel,tvStatus;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,6 +54,26 @@ public class BerandaFragment extends Fragment{
 
     private void eventFragmentAkun() {
         initialize();
+
+        userRefs.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                {
+                    String saldo = dataSnapshot.child("saldo").getValue().toString();
+                    String level = dataSnapshot.child("level").getValue().toString();
+
+                    tvSaldo.setText("Rp. " + saldo);
+                    tvLevel.setText(" " + level);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public void initialize(){
@@ -51,7 +83,8 @@ public class BerandaFragment extends Fragment{
         notifikasiFragment = new NotifikasiFragment();
         bottomNavigationView = (BottomNavigationView) getActivity().findViewById(R.id.bottomNavBar);
         bottomNavigationView.setVisibility(View.VISIBLE);
-
+        tvSaldo = (TextView) getActivity().findViewById(R.id.tv_saldo_beranda);
+        tvLevel = (TextView) getActivity().findViewById(R.id.tv_level_beranda);
         btnSetting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,6 +98,11 @@ public class BerandaFragment extends Fragment{
                 sentToNotif();
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        userRefs = FirebaseDatabase.getInstance().getReference().child("User").child(currentUserID);
+
     }
 
     private void sentToSetting(){
